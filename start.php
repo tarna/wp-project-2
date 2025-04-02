c119540adc09c3852293e8e8624f125c1aad32d1
ndermer1 <dermernathan@gmail.com>
2025-04-01 21:29:48 -0400

    Fixed leaderboard to order users correctly and styled start

--- start.php
+++ start.php
@@ -3,24 +3,47 @@
 <title>Jeopardy!</title> 
 
 <style>
+    body{
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: blue;
+        font-family: "Times New Roman", Times, serif;
+    }
+
     .login{
         display:flex;
         flex-direction: column;
         flex-wrap: nowrap;
         border:2px solid lightslategray;
         width:35%;
+        background-color: lightskyblue;
     }
 
     .leaderboard{
         float:right;
         border:2px solid lightslategray;
         width:35%;
+        background-color: lightskyblue;
+    }
+
+    .page{
+        display: flex;
+        flex-direction:row;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    h1{
+        text-align: center;
+        font-weight: bold;
     }
 </style>
 </head> 
 <body> 
     <h1>Welcome to Jeopardy!</h1>
-
+    <div class="page">
     <div class="login">
         <div>
             <h1>Login</h1>
@@ -28,15 +51,19 @@
 
         <form action="https://codd.cs.gsu.edu/~ndermer1/WP/Project/02/GameBoard.php" method="post">
             <div>
-                Input User Name 1
+                User 1
                 <input type="text" id="user1" name="user1">
             </div>
+            
+            <br>
 
             <div>
-                Input User Name 2
+                User 2
                 <input type="text" id="user2" name="user2">
             </div>
 
+            <br>
+            
             <div>
                 <input type="submit" value="Play!">
             </div>
@@ -44,14 +71,33 @@
     </div>
 
     <?php
-        $homepage = file_get_contents('users.txt' );
-        echo "<div class=leaderboard>";
-            echo "<h1>Leaderboard</h1>";
-            echo "<ol>";
-                echo "$homepage";
-            "</ol>";
-        "</div>";
+    $lines = file('users.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $users = [];
+    
+    //Seperate user and score
+    foreach ($lines as $line){
+        // Use regex to extract user and score
+        if(preg_match('/User (\d+): (\d+)/', $line, $matches)){
+            $user = 'User ' . $matches[1];
+            $score = (int)$matches[2];
+            $users[$user] = $score;
+        }
+    }
+
+    //Sort array by scores in descending order
+    arsort($users);
+
+    //Output sorted useres
+    echo "<div class=leaderboard>";
+    echo "<h1>Leaderboard</h1>";
+    echo "<ol>";
+    foreach ($users as $user => $score) {
+        echo "<li> $user: $score </li>\n";
+    }
+    echo "</ol>";
+    echo "</div>";
     ?>
+    </div>
 
 
 </body> 
